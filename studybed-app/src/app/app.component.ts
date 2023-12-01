973a1fe629684ece02444342fe1e50b241bae038
clemparpa <clem.parpaillon@gmail.com>
2023-12-01 19:37:38 +0100

    installing angular app depths

--- studybed-app/src/app/app.component.ts
+++ studybed-app/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
+import { MarkdownComponent } from "ngx-markdown";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MarkdownComponent],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
+    <div class="prose lg:prose-xl">
+      <h1 class="text-red-300">Welcome to {{ title }}!</h1>
 
+      <markdown katex> $ x^2 + y^2 = z^2 $ </markdown>
+    </div>
     <router-outlet></router-outlet>
   `,
   styles: [],
--- studybed-app/src/app/app.config.ts
+++ studybed-app/src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig } from "@angular/core";
+import { provideHttpClient, HttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { provideMarkdown } from "ngx-markdown";
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from "./app.routes";
+import { provideClientHydration } from "@angular/platform-browser";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideMarkdown({
+      loader: HttpClient,
+    }),
+  ],
 };

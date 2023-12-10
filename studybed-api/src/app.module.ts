a8d3bf72f8782c09ff006359c05cd8e34c369481
clemparpa <clem.parpaillon@gmail.com>
2023-12-10 14:14:36 +0100

    refacto and user module

--- studybed-api/src/app.module.ts
+++ studybed-api/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { CourseModule } from './courses/course.module';
+import { CourseModule } from './features/courses/course.module';
 
 @Module({
   imports: [CourseModule],
